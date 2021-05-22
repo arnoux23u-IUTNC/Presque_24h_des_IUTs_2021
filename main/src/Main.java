f1608e6dcdd1b18b2fd87d1c8d4a46617db232b5
AntoineRaoult <antoine-raoult@orange.fr>
2021-05-22 08:23:53 +0200

    main

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,2 +1,10 @@
+import network.Client;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Client client = Client.getInstance();
+        client.connect("127.0.0.1",2121);
+    }
 }
